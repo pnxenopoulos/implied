b1bdaa0003cc13b66f62f5436d94f001c3e69674
ofrighil <32347630+ofrighil@users.noreply.github.com>
2023-08-21 21:56:24 -0400

    add calculate_hold function (#11)

--- src/odds/mod.rs
+++ src/odds/mod.rs
@@ -2,6 +2,52 @@
 //!
 //! Different odds formats and conversion methods.
 
+use crate::probability::ToProbability;
+
 // Odds types
 pub mod american;
 pub mod decimal;
+
+pub fn calculate_hold<T: ToProbability>(odds: &[T]) -> f64 {
+    odds.iter()
+        .map(|o| o.to_probability().unwrap().value)
+        .sum::<f64>()
+        - 1.0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::odds::american::AmericanOdds;
+    use crate::odds::calculate_hold;
+    use crate::odds::decimal::DecimalOdds;
+
+    #[test]
+    fn test_hold_no_hold() {
+        let odds = vec![
+            AmericanOdds::new(-102).unwrap(),
+            AmericanOdds::new(102).unwrap(),
+        ];
+
+        assert_eq!(calculate_hold(&odds), 0.0);
+    }
+
+    #[test]
+    fn test_hold_standard_hold_american() {
+        let odds = vec![
+            AmericanOdds::new(-110).unwrap(),
+            AmericanOdds::new(-110).unwrap(),
+        ];
+
+        assert_eq!(calculate_hold(&odds), 0.04761904761904767);
+    }
+
+    #[test]
+    fn test_hold_standard_hold_decimal() {
+        let odds = vec![
+            DecimalOdds::new(1.0).unwrap(),
+            DecimalOdds::new(2.0).unwrap(),
+        ];
+
+        assert_eq!(calculate_hold(&odds), 0.5);
+    }
+}
