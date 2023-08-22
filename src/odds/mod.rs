39ad90960e884ab494e167ea587e3f90254f379c
ofrighil <eugene.ofriel@gmail.com>
2023-08-21 21:51:07 -0400

    address Peter's comments

--- src/odds/mod.rs
+++ src/odds/mod.rs
@@ -19,9 +19,10 @@ pub fn calculate_hold<T: ToProbability>(odds: &[T]) -> f64 {
 mod tests {
     use crate::odds::american::AmericanOdds;
     use crate::odds::calculate_hold;
+    use crate::odds::decimal::DecimalOdds;
 
     #[test]
-    fn test_hold_1() {
+    fn test_hold_no_hold() {
         let odds = vec![
             AmericanOdds::new(-102).unwrap(),
             AmericanOdds::new(102).unwrap(),
@@ -31,12 +32,22 @@ mod tests {
     }
 
     #[test]
-    fn test_hold_2() {
+    fn test_hold_standard_hold_american() {
         let odds = vec![
             AmericanOdds::new(-110).unwrap(),
             AmericanOdds::new(-110).unwrap(),
         ];
 
         assert_eq!(calculate_hold(&odds), 0.04761904761904767);
+    }
+
+    #[test]
+    fn test_hold_standard_hold_decimal() {
+        let odds = vec![
+            DecimalOdds::new(1.0).unwrap(),
+            DecimalOdds::new(2.0).unwrap(),
+        ];
+
+        assert_eq!(calculate_hold(&odds), 0.5);
     }
 }
